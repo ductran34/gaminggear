8d2a4b2ec5c3fe9c14e8bce2c85a2f8152a87a99
unknown <trantriduc1vn@gmail.com>
2022-04-30 03:45:19 -0400

    set up styled components

---
+++ client/components/Button.styled.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.div`
+    background: #FE665B;
+    box-sizing: border-box;
+    color: #F8F9FD;
+    cursor: pointer;
+    font-size: 1rem;
+    font-weight: 700;
+    line-height: 1.5rem;
+    outline: 0 solid transparent;
+    padding: 8px 18px;
+    width: fit-content;
+    word-break: break-word;
+    border: 0;
+    margin: 0.25em
+`
---
+++ client/components/Container.styled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 4em;
+    background: lightblue;
+    font-size: 2em;
+`
