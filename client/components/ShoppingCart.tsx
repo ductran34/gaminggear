61ef4f03309a328d90fb1fbe455aada17815a609
DatTran34 <tridat1vn@gmail.com>
2022-07-12 15:21:40 -0400

    woring shopping cart

---
+++ client/components/ShoppingCart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TitleList from './HomePage/TitleList'
+
+function ShoppingCart() {
+  return (
+    <div>
+        <TitleList title='SHOPPING CART'></TitleList>
+    </div>
+  )
+}
+
+export default ShoppingCart
